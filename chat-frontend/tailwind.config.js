29d17e7af6e08c9f4d3b626338ab78bcc77ff53b
Timothy <timothy.aknine@gmail.com>
2025-02-02 17:26:33 +0200

    to_github

--- chat-frontend/tailwind.config.js
+++ chat-frontend/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // 👉 Vérifie bien que ton projet est dans /src
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // /src
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
